e84e5f2832dee3f3265b660879875b097e35c5a6
k4keye <vps32@naver.com>
2020-07-08 02:58:06 +0900

    first commit

---
+++ LodingPage/LodingPage/LodingPage/SplashPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace LodingPage
+{
+    class SplashPage : ContentPage
+    {
+        Image splashImage;
+
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            //false 절대 레이아웃을 사용하고 만든다.
+            var sub = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                Source = "Twitter.png",
+                WidthRequest = 100,
+                HeightRequest = 100
+            };
+            AbsoluteLayout.SetLayoutFlags(splashImage,
+                AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage,
+                new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            sub.Children.Add(splashImage);
+
+            this.BackgroundColor = Color.FromHex("#50ABF1");
+            this.Content = sub;
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await splashImage.ScaleTo(1, 2000);
+            await splashImage.ScaleTo(0.9, 1500, Easing.Linear);
+            await splashImage.ScaleTo(150, 1200, Easing.Linear);
+            Application.Current.MainPage = new NavigationPage(new Page1());
+        }
+    }
+}
